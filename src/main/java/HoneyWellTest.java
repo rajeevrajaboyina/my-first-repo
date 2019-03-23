e83a79a64a324a00b998a24b817ee8a39dcd9518
Rajeev <Rajeev@Windows>
2019-03-23 13:26:50 +0530

    my second commit as committer rajeev

--- src/main/java/HoneyWellTest.java
+++ src/main/java/HoneyWellTest.java
@@ -4,6 +4,7 @@ public class HoneyWellTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is for honeywell test...");
+		System.out.println("This is for honeywell test2...");
 	}
 
 }
